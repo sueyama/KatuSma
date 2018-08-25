ba0c3781f24eba2f86b0e6b17e21d054f27569d7
上山　俊佑 <>
2018-08-25 13:35:34 +0900

    初期版コミット

---
+++ KatuSma/News5ViewController.swift
@@ -0,0 +1,268 @@
+//
+//  News5ViewController.swift
+//  KatuSma
+//
+//  Created by 上山　俊佑 on 2018/05/09.
+//  Copyright © 2018年 Shunsuke Ueyama. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class News5ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIWebViewDelegate, XMLParserDelegate {
+    
+    var urlArray = [String]()
+    
+    var tableView:UITableView = UITableView()
+    var refreshControl:UIRefreshControl!
+    var webView:UIWebView = UIWebView()
+    var goButton:UIButton!
+    var backButton:UIButton!
+    var cancelButton:UIButton!
+    var dotsView:DotsLoader! = DotsLoader()
+    var parser = XMLParser()
+    var totalBox = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = String()
+    var titleString = NSMutableString()
+    var linkString = NSMutableString()
+    var urlString = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 背景画像をつくる
+        let imageView = UIImageView()
+        imageView.frame = self.view.bounds
+        imageView.image = UIImage(named: "5.jpg")
+        self.view.addSubview(imageView)
+        
+        // 引っ張って更新
+        refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor.white
+        refreshControl.addTarget(self, action: #selector(refresh), for: UIControlEvents.valueChanged)
+        
+        // TabaleViewを作成する
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 54.0)
+        tableView.backgroundColor = UIColor.clear
+        tableView.addSubview(refreshControl)
+        self.view.addSubview(tableView)
+        
+        // webView
+        webView.frame = tableView.frame
+        webView.delegate = self
+        webView.scalesPageToFit = true
+        webView.contentMode = .scaleAspectFit
+        self.view.addSubview(webView)
+        webView.isHidden = true
+        
+        // 1つ進むボタン
+        goButton = UIButton()
+        goButton.frame = CGRect(x: self.view.frame.size.width - 50, y: self.view.frame.size.height - 120, width: 50, height: 50)
+        goButton.setImage(UIImage(named: "go.png"), for: .normal)
+        goButton.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
+        self.view.addSubview(goButton)
+        
+        // 1つ戻るボタン
+        backButton = UIButton()
+        backButton.frame = CGRect(x: 10, y: self.view.frame.size.height - 120, width: 50, height: 50)
+        backButton.setImage(UIImage(named: "back.png"), for: .normal)
+        backButton.addTarget(self, action: #selector(backPage), for: .touchUpInside)
+        self.view.addSubview(backButton)
+        
+        // キャンセルボタン
+        cancelButton = UIButton()
+        cancelButton.frame = CGRect(x: 10, y: 80, width: 50, height: 50)
+        cancelButton.setImage(UIImage(named: "cancel.png"), for: .normal)
+        cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        self.view.addSubview(cancelButton)
+        
+        goButton.isHidden = true
+        backButton.isHidden = true
+        cancelButton.isHidden = true
+        
+        // ドットビュー
+        dotsView.frame = CGRect(x: 0, y: self.view.frame.size.height/3, width: self.view.frame.size.width, height: 100)
+        dotsView.dotsCount = 5
+        dotsView.dotsRadius = 10
+        self.view.addSubview(dotsView)
+        
+        dotsView.isHidden = true
+        
+        // xmlを解析する
+        let url:String = "https://rss.allabout.co.jp/aa/latest/ch/beautydiet/"
+        let urlToSend:URL = URL(string: url)!
+        parser = XMLParser(contentsOf: urlToSend)!
+        totalBox = []
+        parser.delegate = self
+        parser.parse()
+        tableView.reloadData()
+        
+    }
+    
+    @objc func refresh(){
+        perform(#selector(delay), with: nil, afterDelay: 2.0)
+    }
+    
+    @objc func delay(){
+        // xmlを解析する
+        let url:String = "https://rss.allabout.co.jp/aa/latest/ch/beautydiet/"
+        let urlToSend:URL = URL(string: url)!
+        parser = XMLParser(contentsOf: urlToSend)!
+        totalBox = []
+        parser.delegate = self
+        parser.parse()
+        tableView.reloadData()
+        refreshControl.endRefreshing()
+        
+    }
+    
+    @objc func nextPage(){
+        webView.goForward()
+    }
+    
+    @objc func backPage(){
+        webView.goBack()
+    }
+    
+    @objc func cancel(){
+        webView.isHidden = true
+        goButton.isHidden = true
+        backButton.isHidden = true
+        cancelButton.isHidden = true
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return totalBox.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        
+        cell.selectionStyle = .none
+        cell.backgroundColor = UIColor.clear
+        cell.textLabel?.text = (totalBox[indexPath.row] as AnyObject).value(forKey: "title") as? String
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15.0)
+        cell.textLabel?.textColor = UIColor.white
+        
+        cell.detailTextLabel?.text = (totalBox[indexPath.row] as AnyObject).value(forKey: "link") as? String
+        cell.detailTextLabel?.font = UIFont.boldSystemFont(ofSize: 9.0)
+        cell.detailTextLabel?.textColor = UIColor.white
+        
+        let urlStr = urlArray[indexPath.row].addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url:URL = URL(string:urlStr)!
+        
+        cell.imageView?.sd_setImage(with: url,placeholderImage: UIImage(named:"placeholderImage.png"))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // WebViewを表示させる
+        let linkURL = (totalBox[indexPath.row] as AnyObject).value(forKey: "link") as? String
+        //        let urlStr = linkURL?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        let url:URL = URL(string: linkURL!)!
+        let urlRequest = NSURLRequest(url: url)
+        webView.loadRequest(urlRequest as URLRequest)
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        dotsView.isHidden = false
+        dotsView.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        dotsView.isHidden = true
+        dotsView.stopAnimating()
+        webView.isHidden = false
+        goButton.isHidden  = false
+        backButton.isHidden = false
+        cancelButton.isHidden = false
+    }
+    
+    
+    //タグを見つけた時
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        element = elementName
+        
+        if element == "item"{
+            elements = NSMutableDictionary()
+            elements = [:]
+            titleString = NSMutableString()
+            titleString = ""
+            linkString = NSMutableString()
+            linkString = ""
+            urlString = String()
+            urlString = ""
+//        }else if element == "media:thumbnail"{
+//
+//            urlString = attributeDict["url"]!
+//            urlArray.append(urlString)
+            
+        }
+    }
+    
+    
+    
+    //タグの間にデータがあったとき(開始タグと終了タグでくくられた箇所にデータが存在したときに実行されるメソッド)
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if element == "title"{
+            let arr1:[String] = string.components(separatedBy: "\n")
+            titleString.append(arr1[0])
+        }else if element == "link"{
+            let arr2:[String] = string.components(separatedBy: "\n")
+            linkString.append(arr2[0])
+        }else if element == "description"{
+            let arr3:[String] = string.components(separatedBy: "src=\"")
+            let arr4:String!
+            if arr3.count == 2 {
+                arr4 = arr3[1].components(separatedBy: "\" border")[0]
+            } else {
+                arr4 = arr3[0]
+            }
+            if arr4.contains("http") {
+                urlString.append(arr4)
+                urlArray.append(urlString)
+            }
+        }
+    }
+    
+    
+    //タグの終了を見つけたとき
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        //itemという要素の中にあるなら、
+        if elementName == "item"{
+            //titleString(linkString)の中身が空でないなら、
+            if titleString != ""{
+                //elementsにキー値を付与しながらtitleString(linkString)をセットする
+                elements.setObject(titleString, forKey: "title" as NSCopying)
+            }
+            if linkString != ""{
+                //elementsにキー値を付与しながらtitleString(linkString)をセットする
+                elements.setObject(linkString, forKey: "link" as NSCopying)
+            }
+            
+            elements.setObject(urlString, forKey: "description" as NSCopying)
+            
+            //totalBoxの中にelementsを入れる
+            totalBox.add(elements)
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ KatuSma/ViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  KatuSma
+//
+//  Created by 上山　俊佑 on 2018/05/08.
+//  Copyright © 2018年 Shunsuke Ueyama. All rights reserved.
+//
+
+import UIKit
+import WBSegmentControl
+
+class ViewController: UIViewController {
+    
+    var segmentControl: WBSegmentControl!
+    var viewPages = UIView()
+    var viewLabel = UILabel()
+    var pagesController: UIPageViewController!
+    var pages: [UIViewController] = []
+    
+    override func loadView() {
+        super.loadView()
+        
+        initSegmentControl()
+        initPagesController()
+        
+        view.addSubview(segmentControl)
+        view.addSubview(viewPages)
+        viewPages.addSubview(pagesController.view)
+        view.addSubview(viewLabel)
+        
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[segmentControl]|", options: .alignAllLeading, metrics: nil, views: ["segmentControl": segmentControl]))
+        view.addConstraint(NSLayoutConstraint(item: segmentControl, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: segmentControl, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 0, constant: 40))
+        
+        viewPages.gestureRecognizers = pagesController.gestureRecognizers
+        viewPages.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[viewPages]|", options: .alignAllLeading, metrics: nil, views: ["viewPages": viewPages]))
+        view.addConstraint(NSLayoutConstraint(item: viewPages, attribute: .top, relatedBy: .equal, toItem: segmentControl, attribute: .bottom, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: viewPages, attribute: .bottom, relatedBy: .equal, toItem: viewLabel, attribute: .top, multiplier: 1, constant: 40))
+        
+        pagesController.view.translatesAutoresizingMaskIntoConstraints = false
+        viewPages.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[pagesView]|", options: .alignAllLeading, metrics: nil, views: ["pagesView": pagesController.view]))
+        viewPages.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[pagesView]|", options: .alignAllFirstBaseline, metrics: nil, views: ["pagesView": pagesController.view]))
+        
+        viewLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[viewLabel]|", options: .alignAllLeading, metrics: nil, views: ["viewLabel": viewLabel]))
+        view.addConstraint(NSLayoutConstraint(item: viewLabel, attribute: .top, relatedBy: .equal, toItem: self.bottomLayoutGuide, attribute: .top, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: viewLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 0, constant: 40))
+        
+        viewLabel.textAlignment = .center
+        viewLabel.textColor = UIColor.black
+        viewLabel.backgroundColor = UIColor.white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        segmentControl.selectedIndex = 0
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initSegmentControl() {
+        segmentControl = WBSegmentControl()
+        segmentControl.segments = [
+            TextSegment(text: "Skincare"),
+            TextSegment(text: "Disease"),
+            TextSegment(text: "Bodycare"),
+            TextSegment(text: "Health"),
+            TextSegment(text: "Beautydiet"),
+        ]
+        segmentControl.style = .rainbow
+        segmentControl.segmentTextFontSize = 12.0
+        segmentControl.segmentTextBold = false
+        segmentControl.rainbow_colors = [
+            UIColor(red:0.91, green:0.18, blue:0.24, alpha:1.00),
+            UIColor(red:1.00, green:0.71, blue:0.26, alpha:1.00),
+            UIColor(red:0.47, green:0.78, blue:0.97, alpha:1.00)
+        ]
+        segmentControl.delegate = self
+        segmentControl.segmentMinWidth = 10.0
+    }
+    
+    func initPagesController() {
+        pagesController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pagesController.dataSource = self
+        pagesController.delegate = self
+        
+        segmentControl.segments.enumerated().forEach { (index, _) in
+            let vc1:UIViewController = News1ViewController()
+            let vc2:UIViewController = News2ViewController()
+            let vc3:UIViewController = News3ViewController()
+            let vc4:UIViewController = News4ViewController()
+            let vc5:UIViewController = News5ViewController()
+            
+            pages.append(vc1)
+            pages.append(vc2)
+            pages.append(vc3)
+            pages.append(vc4)
+            pages.append(vc5)
+        }
+    }
+    
+}
+
+extension ViewController: WBSegmentControlDelegate {
+    func segmentControl(_ segmentControl: WBSegmentControl, selectIndex newIndex: Int, oldIndex: Int) {
+        let targetPages = [pages[newIndex]]
+        let direction = ((newIndex > oldIndex) ? UIPageViewControllerNavigationDirection.forward : UIPageViewControllerNavigationDirection.reverse)
+        pagesController.setViewControllers(targetPages, direction: direction, animated: true, completion: nil)
+        
+        if let selectedSegment = segmentControl.selectedSegment as? TextSegment {
+            viewLabel.text = selectedSegment.otherAttr
+        }
+    }
+}
+
+extension ViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let index = pages.index(of: viewController)!
+        return index > 0 ? pages[index - 1] : nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = pages.index(of: viewController)!
+        return index < pages.count - 1 ? pages[index + 1] : nil
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
+
+extension ViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed == false {
+            guard let targetPage = previousViewControllers.first else {
+                return
+            }
+            guard let targetIndex = pages.index(of: targetPage) else {
+                return
+            }
+            segmentControl.selectedIndex = targetIndex
+            pageViewController.setViewControllers(previousViewControllers, direction: .reverse, animated: true, completion: nil)
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        guard let targetPage = pendingViewControllers.first else {
+            return
+        }
+        guard let targetIndex = pages.index(of: targetPage) else {
+            return
+        }
+        segmentControl.selectedIndex = targetIndex
+    }
+}
